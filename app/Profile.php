5b414e9b6fcc894a60d7aac800350e88d59a6016
Abdellah Ahmed <abs12050@hotmail.com>
2019-09-03 12:27:26 +0600

    default profile pic added

--- app/Profile.php
+++ app/Profile.php
@@ -7,9 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Profile extends Model
 {
     protected $guarded = [];
-    
+
     public function user() {
         
         return $this->belongsTo(User::class);
+    }
+
+    public function profileImage() {
+        $imagePath = ($this->image) ? $this->image : 'profile/5j3sN6znuLXwVyDLXb5AFuc2tiu5NmCxerYO859q.png';
+        return  '/storage/' . $imagePath;
     }
 }
